8cfebaf2d4189aaf2bbdfe919f774b8dd085da3a
Mirons23 <miron.serebrennikov@mail.ru>
2020-02-23 16:45:23 +0300

    Add realization

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// Copyright 2020 Serebrennikov
+#include <iostream>
+#include <clocale>
+#include "Point.h"
+
+using namespace std;
+
+int main() {
+  Point tochka(5.6, 2.1, -7.4);
+  tochka.output();
+  system("pause");
+}
